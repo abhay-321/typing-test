2e0d69b37ef607b039d8fbaeca5c2c807c7ffa21
Abhay Ahire <abhayahire3@gmail.com>
2024-05-25 18:01:56 +0530

    add informative text

--- src/components/DifficultySelector.js
+++ src/components/DifficultySelector.js
@@ -3,6 +3,7 @@ import React from 'react';
 const DifficultySelector = ({ onSelectLevel }) => {
   return (
     <div className="difficulty-selector">
+      <h3>Please choose a level to begin...</h3>  
       <button onClick={() => onSelectLevel('Beginner')}>Beginner</button>
       <button onClick={() => onSelectLevel('Medium')}>Medium</button>
       <button onClick={() => onSelectLevel('Advanced')}>Advanced</button>
